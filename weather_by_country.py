959193abeb5671c8b0d7ef5175a427a2dc2e0724
maromoz <maromohevzion@gmail.com>
2016-07-28 17:06:11 +0300

    Add existing file

--- weather_by_country.py
+++ weather_by_country.py
@@ -17,18 +17,15 @@ print "The cities inside the country you have choosen"
 for line in data:
 	if country == line[-3]+line[-2]:
 		elements = line.split()
-		cities =  " ".join(elements[1:-3]) 
-		new_line = i,cities
-		print new_line
+		city =  " ".join(elements[1:-3]) 
+		cities_list.append(city)
+		print i,city
 		i+=1
-		cities_list += new_line
 user_choice = raw_input("Please type the nubmer of city you would like to check the weather: ")
-for i in cities_list:
-	if str(user_choice) == str(i):
-		s = int(user_choice) + 1
-		s = 2*s - 3
-		city = cities_list[s]
-		print "The city you have chosen is " + str(city)
+user_choice = int(user_choice) - 1
+city = cities_list[user_choice]
+city = city.strip().replace(" ", "-")
+
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=9aa4492b034cef3250be6f72629b73e9'
 values = {}
